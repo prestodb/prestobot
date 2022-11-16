a35ec85537b5dee498547969220cee70df3fe993
v-jizhang <jzhang@openteams.com>
2022-11-16 13:13:49 -0800

    Task: Tag a PR based on the files modified

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { rerunFailedTests, assignReviewersToPullRequest, welcomeNewContributors, validateCommits } = require('./pull_request/pull_requests')
+const { rerunFailedTests, assignReviewersToPullRequest, welcomeNewContributors, validateCommits, tagPullRequest } = require('./pull_request/pull_requests')
 
 /**
  * This is the main entrypoint to your Probot app
@@ -29,8 +29,10 @@ module.exports = (app) => {
     await welcomeNewContributors(context);
     await validateCommits(context);
     await assignReviewersToPullRequest(context);
+    await tagPullRequest(context);
   });
-*/
+  */
+
   app.on(
       //["commit_comment.created",
       //"pull_request_review_comment.created",
