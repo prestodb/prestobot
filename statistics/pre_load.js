10f0f4008a466e96855c101c9bd3d717dbc08993
v-jizhang <jzhang@openteams.com>
2023-02-12 10:22:20 -0800

    Skip PRs if already uploaded

--- statistics/pre_load.js
+++ statistics/pre_load.js
@@ -16,6 +16,8 @@ const updatePreloadLog = `INSERT INTO logs
     VALUES('preload', $1, $2);`;
 const selectPrNumOfLastLoad = `SELECT * FROM logs
     WHERE event = 'preload' and message = $1;`;
+const selectLastPrLoaded = `SELECT * FROM pull_requests
+    ORDER by id DESC limit 1;`;
 
 async function preLoadPullRequestData(app)
 {
@@ -68,6 +70,11 @@ async function getStartPrNumber(app)
         if(resCount.rowCount > 2) {
             // Tried at least 3 time, this PR cannot be read, skip it.
             startNumber++;
+        }
+
+        const resLastPr = await client.query(selectLastPrLoaded);
+        if (resLastPr.rowCount === 1 && startNumber <= resLastPr.rows[0].id) {
+            startNumber = resLastPr.rows[0].id + 1;
         }
         let endNumber = -1;
         const resEnd = await client.query(selectPreloadEnd);
